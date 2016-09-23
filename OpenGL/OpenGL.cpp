aa20d3c02cb9bc949b2b3549f5a43844cf11b9b7
allenwm <506763070@qq.com>
2016-09-23 11:31:01 +0800

    draw dotted line

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -6,13 +6,22 @@ void display(void)
 {
 	GLfloat x;
 	glClear(GL_COLOR_BUFFER_BIT);
-	//glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
+	glPointSize(5.0f);
+	glBegin(GL_POINTS);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+	glEnd();
+
+	glEnable(GL_LINE_STIPPLE);
+	glLineStipple(2, 0x0F0F);
+	glLineWidth(2.0f);
 	glBegin(GL_LINES);
 		glVertex2f(-1.0f, 0.0f);
 		glVertex2f(1.0f, 0.0f);
 		glVertex2f(0.0f, -1.0f);
 		glVertex2f(-0.0f, 1.0f);
 	glEnd();
+	glDisable(GL_LINE_STIPPLE);
 
 	glBegin(GL_LINE_STRIP);
 	for ( x = -1.0f / factor; x < 1.0 / factor; x+=0.01f)
