d1960c54b73acbab533ffe5325b99bab5fb58a29
allenwm <506763070@qq.com>
2016-09-23 11:00:11 +0800

    draw pentacle

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -6,11 +6,24 @@ const GLfloat Pi = 3.1415926536f;
 
 void display(void)
 {
+	GLfloat a = 1 / (2 - 2 * cos(72 * Pi / 180));
+	GLfloat bx = a * cos(18 * Pi / 180);
+	GLfloat by = a * sin(18 * Pi / 180);
+	GLfloat cy = -a * cos(18 * Pi / 180);
+	GLfloat
+		PointA[2] = { 0, a },
+		PointB[2] = { bx, by },
+		PointC[2] = { 0.5, cy },
+		PointD[2] = { -0.5, cy },
+		PointE[2] = { -bx, by };
 	glClear(GL_COLOR_BUFFER_BIT);
 	//glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
-	glBegin(GL_POLYGON);
-	for (int i = 0; i<n; ++i)
-		glVertex2f(R*cos(2 * Pi / n*i), R*sin(2 * Pi / n*i));
+	glBegin(GL_LINE_LOOP);
+		glVertex2fv(PointA);
+		glVertex2fv(PointC);
+		glVertex2fv(PointE);
+		glVertex2fv(PointB);
+		glVertex2fv(PointD);
 	glEnd();
 	glFlush();
 }
